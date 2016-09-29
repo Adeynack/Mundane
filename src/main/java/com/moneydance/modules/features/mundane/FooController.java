652ce0c68c56f6e4b9cacb9627514239c521d02a
David Dufour-Boivin <david.dufour-boivin@zalando.de>
2016-09-30 00:13:14 +0200

    Use JavaFX from Swing.

---
+++ src/main/java/com/moneydance/modules/features/mundane/FooController.java
@@ -0,0 +1,14 @@
+package com.moneydance.modules.features.mundane;
+
+import javafx.event.ActionEvent;
+import javafx.scene.text.Text;
+
+public class FooController {
+
+    public Text actionTarget;
+
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+
+    }
+
+}
--- src/main/java/com/moneydance/modules/features/mundane/FullTextTransactionSearchWindow.java
+++ src/main/java/com/moneydance/modules/features/mundane/FullTextTransactionSearchWindow.java
@@ -3,6 +3,16 @@ package com.moneydance.modules.features.mundane;
 import com.infinitekind.moneydance.model.AbstractTxn;
 import com.moneydance.apps.md.controller.FeatureModuleContext;
 import com.moneydance.awt.AwtUtil;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import net.miginfocom.layout.AC;
 import net.miginfocom.layout.CC;
 import net.miginfocom.layout.LC;
@@ -13,8 +23,10 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 class FullTextTransactionSearchWindow extends JFrame {
@@ -49,10 +61,11 @@ class FullTextTransactionSearchWindow extends JFrame {
 //                "[grow, fill]",
                 new AC().grow().fill().gap()
                         .shrink(),
-//                "[][grow, fill][r]"
+//                "[][grow, fill][]"
                 new AC().shrink().gap()
                         .grow().fill().gap()
-                        .shrink()
+                        .shrink().gap()
+                        .fill()
         ));
 
         txtSearchInput = new JTextField();
@@ -82,7 +95,35 @@ class FullTextTransactionSearchWindow extends JFrame {
         final JButton btnClose = new JButton(actionClose);
         pnlButtons.add(btnClose);
 
-        root.add(pnlButtons, new CC().spanX());
+        root.add(pnlButtons, new CC().spanX().wrap());
+
+        final JFXPanel fxPanel = new JFXPanel();
+        root.add(fxPanel, new CC().spanX());
+        Platform.runLater(() -> {
+//            Group rootGrp = new Group();
+//            Scene scene = new Scene(rootGrp, Color.ALICEBLUE);
+//
+//            Text text = new Text();
+//            text.setX(40);
+//            text.setY(100);
+//            text.setFont(new Font(25));
+//            text.setText("Welcome JavaFX!");
+//            rootGrp.getChildren().add(text);
+//
+//            Button button = new Button("Clear search");
+//            button.setOnAction(e ->
+//                    SwingUtilities.invokeLater(() ->
+//                            txtSearchInput.setText("")));
+//            rootGrp.getChildren().add(button);
+
+            try {
+                final Parent parent = FXMLLoader.load(getClass().getResource("Foo.fxml"));
+                final Scene scene = new Scene(parent);
+                fxPanel.setScene(scene);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
         setSize(500, 400);
         getContentPane().add(root);
