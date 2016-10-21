ce445e4b37b3e54905c28a1f15be3b93a1c03e60
David Dufour-Boivin <david.dufour-boivin@zalando.de>
2016-10-21 12:05:23 +0200

    Listen to event (to learn what's possible to listen to with the MD extension API).

--- src/main/scala/com/moneydance/modules/features/mundane/Main.scala
+++ src/main/scala/com/moneydance/modules/features/mundane/Main.scala
@@ -4,6 +4,7 @@ import java.awt.{Font, Image, Toolkit}
 import java.io.ByteArrayOutputStream
 
 import com.github.adeynack.scala.swing.FrameManager
+import com.infinitekind.moneydance.model._
 import com.moneydance.apps.md.controller.FeatureModule
 import play.api.libs.json._
 
@@ -14,9 +15,43 @@ class Main extends FeatureModule {
   private implicit lazy val context = getContext
   private val fullTextTransactionSearch = new FrameManager(() => new FullTextTransactionSearchFrame)
 
+
   override def init(): Unit = {
     context.registerFeature(this, invokeStr.fullTextSearch, icon, "Full Text Transaction Search")
     context.registerFeature(this, invokeStr.accountsToJson, icon, "Export account list to JSON in the clipboard")
+  }
+
+  private val accountBookListener = new AccountBookListener {
+
+    override def accountBookDataUpdated(accountBook: AccountBook): Unit =
+      System.err.println(s"AccountBookListener::accountBookDataUpdated accountBook = $accountBook")
+
+    override def accountBookDataReplaced(accountBook: AccountBook): Unit =
+      System.err.println(s"AccountBookListener::accountBookDataReplaced accountBook = $accountBook")
+
+  }
+
+  private val accountListener = new AccountListener {
+
+    override def accountAdded(account: Account, account1: Account): Unit =
+      System.err.println(s"AccountListener::accountAdded account = $account account1 = $account1")
+
+    override def accountDeleted(account: Account, account1: Account): Unit =
+      System.err.println(s"AccountListener::accountDeleted account = $account account1 = $account1")
+
+    override def accountBalanceChanged(account: Account): Unit =
+      System.err.println(s"AccountListener::accountBalanceChanged account = $account")
+
+    override def accountModified(account: Account): Unit =
+      System.err.println(s"AccountListener::accountModified account = $account")
+
+  }
+
+  val fileListener = new MDFileListener {
+
+    override def dirtyStateChanged(account: Account): Unit =
+      System.err.println(s"MDFileListener::dirtyStateChanged account = $account")
+
   }
 
   override def getName = "Mundane"
@@ -56,6 +91,18 @@ class Main extends FeatureModule {
     }
   }
 
+  override def handleEvent(appEvent: String): Unit = {
+    appEvent match {
+      case "md:account:root" =>
+        System.err.println(s"Main::handleEvent appEvent = $appEvent")
+        context.getCurrentAccountBook.addListener(accountBookListener)
+        context.getCurrentAccountBook.addAccountListener(accountListener)
+        context.getCurrentAccountBook.addFileListener(fileListener)
+      case _ =>
+        System.err.println(s"Main::handleEvent appEvent = $appEvent")
+    }
+    super.handleEvent(appEvent)
+  }
 }
 
 object Main {
