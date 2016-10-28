c52421fad6e2c8177ef0128c1e80dddb87f6c4de
David Dufour-Boivin <david.dufour-boivin@zalando.de>
2016-10-28 21:02:07 +0200

    Cache the JsonLocalStorage value instead of re-reading-and-parsing it every time.

--- src/main/scala/com/moneydance/modules/scalamd/JsonLocalStorage.scala
+++ src/main/scala/com/moneydance/modules/scalamd/JsonLocalStorage.scala
@@ -31,9 +31,13 @@ class JsonLocalStorage[T](val key: String, default: => T, context: FeatureModule
 
   private val localStorage = context.getCurrentAccountBook.getLocalStorage
 
+  private var cached: Option[T] = None
+
   def set(value: T): Unit = {
-    val toJson = Json.toJson(value).toString
-    localStorage.put(key, toJson)
+    val toJson = Json.toJson(value)
+    System.err.println(s"""Saving to local storage with key "$key" and value ${Json.prettyPrint(toJson)}""")
+    localStorage.put(key, toJson.toString)
+    cached = Some(value)
   }
 
   def update(updater: T => T): Unit = {
@@ -45,26 +49,34 @@ class JsonLocalStorage[T](val key: String, default: => T, context: FeatureModule
   }
 
   def get: T = {
-    Option(localStorage.getString(key, null))
-      .map { content =>
-        Json.parse(content).validate[T] match {
-          case JsSuccess(value, path) => value
-          case JsError(failedPaths) =>
-            System.err.println(s"""Failed to parse setting from local storage under key "$key":""")
-            failedPaths.foreach { case (path, validationErrors) =>
-              System.err.println(s"""  For JSON path "$path":""")
-              validationErrors.foreach { validationError =>
-                System.err.println(s"""    $validationError""")
-              }
+    if (cached.isEmpty) {
+      cached = Some {
+        Option(localStorage.getString(key, null))
+          .map { content =>
+            val contentJsVal = Json.parse(content)
+            contentJsVal.validate[T] match {
+              case JsSuccess(value, path) =>
+                System.err.println(s"""Loaded local storage from key "$key". Read value ${Json.prettyPrint(contentJsVal)}""")
+                value
+              case JsError(failedPaths) =>
+                System.err.println(s"""Failed to parse setting from local storage under key "$key" with value ${Json.prettyPrint(contentJsVal)}""")
+                failedPaths.foreach { case (path, validationErrors) =>
+                  System.err.println(s"""  For JSON path "$path":""")
+                  validationErrors.foreach { validationError =>
+                    System.err.println(s"""    $validationError""")
+                  }
+                }
+                System.err.println(s"""  Using default: $default""")
+                default
             }
-            System.err.println(s"""  Using default: $default""")
+          }
+          .getOrElse {
+            System.err.println(s"""No local storage value found for key "$key". Using default: $default""")
             default
-        }
+          }
       }
-      .getOrElse {
-        System.err.println(s"""No local storage value found for key "$key". Using default: $default""")
-        default
-      }
+    }
+    cached.get
   }
 
 }
